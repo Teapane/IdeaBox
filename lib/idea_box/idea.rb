72d9c5480093f164447081473f08459c38b9c47a
Tyler Long <tyler.stephen.long@gmail.com>
2013-10-17 18:59:49 -0600

    Fixing views

--- lib/idea_box/idea.rb
+++ lib/idea_box/idea.rb
@@ -7,7 +7,7 @@ class Idea
     @description = attributes["description"]
     @rank        = attributes["rank"] || 0
     @id          = attributes ["id"]
-    @tags        = attributes ["tags"]
+    @tags        = attributes ["tags"] || []
   end
 
   def save
